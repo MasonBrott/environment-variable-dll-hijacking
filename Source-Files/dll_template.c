b5719eaa39b98f99090166b18dd4284a1ed9ee48
Mason Brott <47792629+MasonBrott@users.noreply.github.com>
2023-04-11 23:39:44 -0500

    Add files via upload
    Use for walkthrough

---
+++ Source-Files/dll_template.c
@@ -0,0 +1,23 @@
+#include <Windows.h>
+#include <Shlobj.h>
+#include <stdio.h>
+#include <wchar.h>
+
+BOOL WINAPI DllMain(HINSTANCE hinstdll, DWORD fdwreason, LPVOID lpreserved)
+{
+    HANDLE threadhandle;
+    switch (fdwreason)
+    {
+    case DLL_PROCESS_ATTACH:
+        MessageBox(NULL, "Hijacked", "This was run from c:\\evil\\system32", 0);
+        break;
+    break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
